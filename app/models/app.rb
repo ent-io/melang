4a3b65d991d915ee34be04c6fca8ba8caa77a038
Alexander Wenzowski <alexander@wenzowski.com>
2012-06-26 19:16:35 -0400

    add basic validation of apps

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,10 @@
 class App < ActiveRecord::Base
   attr_accessible :git_url, :name
+
+  validates :name,
+    :presence   => :true,
+    :uniqueness => :true
+
+  validates :git_url,
+    :presence   => :true
 end
